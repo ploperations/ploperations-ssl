2ac300b0d02a1e12e87d2d56334725403c6a849b
Gene Liverman <gene.liverman+06301990@puppet.com>
2020-03-02 10:15:00 -0500

    Release prep for 1.0.0
    While generating docs I found that a return type was missing from two
    functions so it was added

--- functions/ensure_newline.pp
+++ functions/ensure_newline.pp
@@ -5,15 +5,15 @@
 # @param [String[0]] string
 #   A string to ensure ends with a new line (aka '\n')
 #
+# @return [String]
+#   Returns a string that ends with a newline (`\n`)
 # @example
 #   file { '/tmp/www.example.com.crt':
 #     ensure  => file,
 #     content => ssl::ensure_newline($ssl::keys['www.example.com']),
 #   }
 #
-function ssl::ensure_newline (
-  String[0] $string,
-) {
+function ssl::ensure_newline(String[0] $string) >> String {
   if $string[-1] == "\n" {
     $string
   } else {
